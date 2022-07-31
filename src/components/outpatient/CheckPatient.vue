d2dd188fb3d131779c40de595f826ce1655e3ed6
mzh <2389182680@qq.com>
2022-07-31 23:24:02 +0800

    7.31

--- src/components/outpatient/CheckPatient.vue
+++ src/components/outpatient/CheckPatient.vue
@@ -6,21 +6,21 @@
 		<hr>
 		<el-form :inline="true">
 			<el-form-item>
-				<el-input v-model="casenumber" placeholder="请输入患者病历号"></el-input>
+				<el-input v-model="cn" placeholder="请输入患者病历号"></el-input>
 			</el-form-item>
 			<el-form-item>
-				<el-input v-model="realname" placeholder="请输入患者姓名"></el-input>
+				<el-input v-model="rn" placeholder="请输入患者姓名"></el-input>
 			</el-form-item>
 			<el-form-item>
 				<el-button @click="search" icon="el-icon-search">搜索</el-button>
 			</el-form-item>
 		</el-form>
 		<el-table :data="patientinfo" style="width: 100%" >
-			<el-table-column prop="index" label="编号" width="200">
+			<el-table-column type="index" label="编号" width="200">
 			</el-table-column>
-			<el-table-column prop="case_number" label="患者病历号" width="200">
+			<el-table-column prop="caseNumber" label="患者病历号" width="200">
 			</el-table-column>
-			<el-table-column prop="real_name" label="患者姓名" width="200px">
+			<el-table-column prop="realName" label="患者姓名" width="200px">
 			</el-table-column>
 			<el-table-column label="操作">
 				<el-button @click="">叫号</el-button>
@@ -40,14 +40,16 @@
 	export default {
 		data() {
 			return {
-				casenumber: '',
-				realname: '',
+				cn: '',
+				rn: '',
 				patientinfo:[],
 			}
 		},
 		methods: {
 			search() {
-
+				this.axios.get('http://localhost:8080/register/selectRegister?cn='+this.cn+'&rn='+this.rn).then((res)=>{
+					this.patientinfo=res.data
+				})
 			},
 			goBack() {
 				this.$router.push("/outpatient")
